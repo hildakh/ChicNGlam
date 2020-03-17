feb56b21e1f6daeac0e6307eea43a25fd7747f24
Hilda <hilda.kh.amiri@gmail.com>
2020-03-16 20:02:08 -0700

    background image of menu item moved to a separate div to make sure it fills the whole container

--- src/components/menu-item/Menu-Item.jsx
+++ src/components/menu-item/Menu-Item.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import './Menu-Item.styles.scss';
 
 const MenuItem = ({ title, id, imageUrl, size }) => (
-  <div
-  style={{
-    backgroundImage: `url(${imageUrl})`
-  }}
-  className={`${size} menu-item`}
-  >
+  <div className={`${size} menu-item`}  >
+    <div
+    style={{
+      backgroundImage: `url(${imageUrl})`
+    }}
+    className="background-image"
+    />
+
+
   <div className="content">
     <h1 className="title">{title.toUpperCase()}</h1>
     <span className="subtitle">SHOP NOW</span>
