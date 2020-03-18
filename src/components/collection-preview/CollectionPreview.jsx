e7545d741a5035a4e5626cb24aa91f7bec259574
Hilda <hilda.kh.amiri@gmail.com>
2020-03-17 20:58:36 -0700

    collection preview component and styling created

---
+++ src/components/collection-preview/CollectionPreview.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './CollectionPreview.styles.scss';
+
+const CollectionPreview = () => {
+  return(
+    <div>
+
+    </div>
+  )
+}
+
+export default CollectionPreview;
