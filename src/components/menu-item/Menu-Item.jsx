7d5975d0ace623913cde028e51e98a87eeb7b455
Hilda <hilda.kh.amiri@gmail.com>
2020-03-17 20:06:17 -0700

    Exported menu item using withRouter to give it access to parent props without having to pass them through generations of children

--- src/components/menu-item/Menu-Item.jsx
+++ src/components/menu-item/Menu-Item.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import './Menu-Item.styles.scss';
+import { withRouter } from 'react-router-dom';
+// withRouter is a higher order component that takes a component as an argument and returns a modified component
 
-const MenuItem = ({ title, id, imageUrl, size }) => (
+const MenuItem = ({ title, id, imageUrl, size, history }) => (
   <div className={`${size} menu-item`}  >
     <div
     style={{
@@ -18,4 +20,5 @@ const MenuItem = ({ title, id, imageUrl, size }) => (
 </div>
 )
 
-export default MenuItem;
+// Exporting the function with withRouter gives this component access to location, match and history props that we had to otherwise pass through children of homepage to have access to
+export default withRouter(MenuItem);
