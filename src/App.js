ec56a42e99174cf6bd4083d56c3c548282c54be2
Hilda <hilda.kh.amiri@gmail.com>
2020-03-17 20:09:07 -0700

    Hats page component temp added in app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/HomePage';
 
+const HatsPage = () =>(
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
+
 function App() {
   return (
     <div>
