b1fa5a57b9afc7df719c74b828a0c442bc13907d
Hilda <hilda.kh.amiri@gmail.com>
2020-03-18 23:02:09 -0700

    filter added to items map function to only render the first 4 elements in the array

--- src/components/collection-preview/CollectionPreview.jsx
+++ src/components/collection-preview/CollectionPreview.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import './CollectionPreview.styles.scss';
+import React from "react";
+import "./CollectionPreview.styles.scss";
 
 const CollectionPreview = ({ title, items }) => {
-  return(
-    <div className='collection-preview'>
-      <h1 className='title'>{title.toUpperCase()}</h1>
-      <div className='preview'>
-        {items.map( item => (
-          <div key={item.id}>
-            {item.name}
-          </div>
+  return (
+    <div className="collection-preview">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <div className="preview">
+        {items
+        .filter((item, index) => index < 4)
+        .map(item => (
+          <div key={item.id}>{item.name}</div>
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default CollectionPreview;
