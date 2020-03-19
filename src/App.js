68fadedda8c40f1e90b17feffecb50d7f16a8afc
Hilda <hilda.kh.amiri@gmail.com>
2020-03-19 16:58:37 -0700

    header added to be rendered in app and header styling added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 import HomePage from "./pages/homepage/HomePage";
 import ShopPage from './pages/shop/Shop';
 import { Switch, Route } from "react-router-dom";
+import Header from "./components/header/Header";
 
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
