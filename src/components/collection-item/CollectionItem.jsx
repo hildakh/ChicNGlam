fe046f89eae82c6c0a3a8b19b4cdf58b80b80804
Hilda <hilda.kh.amiri@gmail.com>
2020-03-19 15:29:51 -0700

    styling added to preview items. shop data bug fixed and image urls uncommented. preview items rendered in collection preview and its props destructured

--- src/components/collection-item/CollectionItem.jsx
+++ src/components/collection-item/CollectionItem.jsx
@@ -2,18 +2,17 @@ import React from "react";
 import "./CollectionItem.styles.scss";
 
 const CollectionItem = ({ id, name, imageUrl, price }) => (
-  <div className="collection-item">
+  <div className="collection-item" >
     <div
       className="image"
       style={{
         backgroundImage: `url(${imageUrl})`
       }}
-    >
+    />
       <div className="collection-footer">
         <span className="name">{name}</span>
-        <span className="name">{price}</span>
+        <span className="price">{price}</span>
       </div>
-    </div>
   </div>
 );
 
