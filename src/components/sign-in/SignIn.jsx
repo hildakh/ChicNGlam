0ee230cbdca9f9121d7678d5426a67ed7f8561ba
Hilda <hilda.kh.amiri@gmail.com>
2020-03-20 00:30:10 -0700

    handle submit function added and modified to store the inserted email and password to state

--- src/components/sign-in/SignIn.jsx
+++ src/components/sign-in/SignIn.jsx
@@ -11,12 +11,17 @@ class SignIn extends Component {
     };
   }
 
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({ email: '', password: ''});
+  }
+
   render() {
     return (
       <div className="signin">
         <h3>I already have an account</h3>
         <span>Sign in with your email and password</span>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <label>Email</label>
           <input type="email" name='email' value={this.state.email} required />
           <label>Password</label>
