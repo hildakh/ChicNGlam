8e720f3ec6d9ae63d9c132cfd0a4094e508d2847
Hilda <hilda.kh.amiri@gmail.com>
2020-03-20 15:48:24 -0700

    comments added. component exported.

--- src/components/form-input/FormInput.jsx
+++ src/components/form-input/FormInput.jsx
@@ -8,9 +8,14 @@ const FormInput = ({ handleChange, label, ...otherProps}) => (
   {
     //if we want to pass a label property, it will be created, otherwise no label will be assigned to the input field
     label ?
-    (<label className={`${otherProps.value.length ? 'shrink' : ''} form-input`}>
+    // label will always have the class form-input but when user has typed something in the input field, class shrink will be added to the style
+    (<label className={`${otherProps.value.length ? 'shrink' : ''} form-input-label`}
+    >
+      {label}
     </label>)
     : null
   }
   </div>
 )
+
+export default FormInput;
