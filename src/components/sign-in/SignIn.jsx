902ffaf065ec975d7af8bd593c6a60c574f89855
Hilda <hilda.kh.amiri@gmail.com>
2020-03-19 22:23:46 -0700

    signin component skeleton finished - class component

--- src/components/sign-in/SignIn.jsx
+++ src/components/sign-in/SignIn.jsx
@@ -1,2 +1,22 @@
-import React from 'react';
-import './SignIn.styles.scss';
+import React, { Component } from "react";
+import "./SignIn.styles.scss";
+
+class SignIn extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  render() {
+    return (
+      <div className="signin">
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
+      </div>
+    );
+  }
+}
