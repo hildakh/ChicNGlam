5a0b84906206b3bc2590664dc473fe574d8b3af0
Hilda <hilda.kh.amiri@gmail.com>
2020-03-19 22:06:14 -0700

    signin and signup component skeleton created and added to app.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import HomePage from "./pages/homepage/HomePage";
 import ShopPage from './pages/shop/Shop';
 import { Switch, Route } from "react-router-dom";
 import Header from "./components/header/Header";
+import SignInSignUp from './pages/signin&signup/SignIn-SignUp';
 
 
 function App() {
   return (
     <div>
       <Header />
+      <SignInSignUp />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
---
+++ src/pages/signin&signup/SignIn-SignUp.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './SignIn-SignUp.styles.scss';
+
+const SignInSignUp = () => (
+  <div className='signin-signup'>SIGN IN</div>
+)
+
+export default SignInSignUp;
