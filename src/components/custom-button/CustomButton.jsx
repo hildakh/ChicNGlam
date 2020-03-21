6146a7fc02e178a0258c28114d8f38c263930343
Hilda <hilda.kh.amiri@gmail.com>
2020-03-20 17:43:35 -0700

    Sign in text added to custom button component used in sign in component. styling for the button added

--- src/components/custom-button/CustomButton.jsx
+++ src/components/custom-button/CustomButton.jsx
@@ -3,7 +3,10 @@ import React from "react";
 import "./CustomButton.styles.scss";
 
 const CustomButton = ({ children, ...otherProps }) => (
+  // making the button reusable for different purposes. Children would be the text showing on the button
   <button className="custom-button" {...otherProps}>
     {children}
   </button>
 );
+
+export default CustomButton;
