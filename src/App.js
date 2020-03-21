ba7b97f0dd3bbfda859f1c50c046560b4f2e79d7
Hilda <hilda.kh.amiri@gmail.com>
2020-03-21 13:06:28 -0700

    imported auth from utils and changed app to a class component to store the current user in the state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/HomePage";
-import ShopPage from './pages/shop/Shop';
+import ShopPage from "./pages/shop/Shop";
 import { Switch, Route } from "react-router-dom";
 import Header from "./components/header/Header";
 import SignInSignUp from "./pages/signin&signup/SignIn-SignUp";
+// used to store the state of the user in the app
+import { auth } from "./firebase/firebase.utils";
 
+class App extends Component {
+  constructor() {
+    super();
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/signup" component={SignInSignUp} />
+        <Switch></Switch>
         <Route exact path="/shop" component={ShopPage} />
-        <Route exact path='/signup' component={SignInSignUp} />
-        <Route exact path='/signin' component={SignInSignUp} />
-      </Switch>
-    </div>
-  );
+        <Route exact path="/signin" component={SignInSignUp} />
+      </div>
+    );
+  }
 }
 
 export default App;
