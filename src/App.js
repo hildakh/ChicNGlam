ea3015ad45d9868b482e001e1c84eb9b7b554ffb
Hilda <hilda.kh.amiri@gmail.com>
2020-03-25 22:06:57 -0700

    stored the snapshot id and data in app state once the userRef and snapshot was created

--- src/App.js
+++ src/App.js
@@ -29,8 +29,14 @@ class App extends Component {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapshot => {
-          console.log(snapshot.data())
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data()
+            }
+          })
         })
+        console.log(this.state)
       }
     })
   }
