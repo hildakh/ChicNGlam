5f13d6e1ff355f2e90bb8ad9ed8b2737a82b5e3a
Hilda <hilda.kh.amiri@gmail.com>
2020-04-03 15:10:14 -0700

    updated the app to redirect to home if the user is signed in. Refactored the user reducer and userActions to use userAction types

--- src/App.js
+++ src/App.js
@@ -7,11 +7,10 @@ import Header from "./components/header/Header";
 import SignInSignUp from "./pages/signin&signup/SignIn-SignUp";
 // used to store the state of the user in the app
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
-import { connect } from 'react-redux';
-import { setCurrentUser } from './redux/user/user.actions';
+import { connect } from "react-redux";
+import { setCurrentUser } from "./redux/user/user.actions";
 
 class App extends Component {
-
   // closing subscription once the app unmounts
   unsubscribeFromAuth = null;
 
@@ -19,22 +18,20 @@ class App extends Component {
     const { setCurrentUser } = this.props;
     // onAuthStateChanged is a method that comes with firebase
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-
-      if(userAuth) {
-
+      if (userAuth) {
         // Getting userRef for an existing user or one that has just been created by google sign in
         const userRef = await createUserProfileDocument(userAuth);
 
         //whenever the usersnapshot updates, we set the userReducer value with the new object
         userRef.onSnapshot(snapshot => {
           setCurrentUser({
-              id: snapshot.id,
-              ...snapshot.data()
-          })
-          })
+            id: snapshot.id,
+            ...snapshot.data()
+          });
+        });
       }
       setCurrentUser(userAuth);
-    })
+    });
   }
 
   componentWillUnmount() {
@@ -44,13 +41,13 @@ class App extends Component {
   render() {
     return (
       <div>
-      <Header />
+        <Header />
 
         <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/signup" component={SignInSignUp} />
-        <Route exact path="/shop" component={ShopPage} />
-        <Route exact path="/signin" component={SignInSignUp} />
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/signup" render={() => this.props.currentUser ? (<Redirect to='/'/>) : (<SignInSignUp />)} />
+          <Route exact path="/shop" component={ShopPage} />
+          <Route exact path="/signin" render={() => this.props.currentUser ? (<Redirect to='/'/>) : (<SignInSignUp />)} />
         </Switch>
       </div>
     );
@@ -59,10 +56,10 @@ class App extends Component {
 
 //getting the userReducer off of state
 const mapStateToProps = ({ user }) => ({
-    currentUser: user.currentUser
-})
+  currentUser: user.currentUser
+});
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
-})
+});
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
