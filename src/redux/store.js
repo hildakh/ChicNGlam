b94b79856b5a6ed6a4f726a0a2dc9e02d57e4fa0
Hilda <hilda.kh.amiri@gmail.com>
2020-04-03 10:15:15 -0700

    middleware array created.

---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+//redux-logger is the middleware to catch actions, console logs them before sending them to the reducer
+
+import rootReducer from './root-reducer';
+
+//store expects an array middleware from redux;
+
+const middlewares = [logger];
+
