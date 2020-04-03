eb007f1d7a43d40d3dad201f66609712e1bf2019
Hilda <hilda.kh.amiri@gmail.com>
2020-04-03 13:36:44 -0700

    currentUser was ripped off from the userReducer user object to be accessed in App to redirect users to pages other than SignIn and SignUp

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/HomePage";
 import ShopPage from "./pages/shop/Shop";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import Header from "./components/header/Header";
 import SignInSignUp from "./pages/signin&signup/SignIn-SignUp";
 // used to store the state of the user in the app
@@ -57,6 +57,10 @@ class App extends Component {
   }
 }
 
+//getting the userReducer off of state
+const mapStateToProps = ({ user }) => ({
+    currentUser: user.currentUser
+})
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
