f6fd5ae63c10d12d4251250e50a0fabb0312e4ee
Hilda <hilda.kh.amiri@gmail.com>
2020-04-07 17:47:56 -0700

    added cart icon and cart dropdown to the header, and implemented cart reducer.

--- src/components/cart-icon/CartIcon.jsx
+++ src/components/cart-icon/CartIcon.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React from "react";
 
-import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+import { connect } from "react-redux";
 
-import './CartIcon.styles.scss';
+import { toggleCartHidden } from "../../redux/cart/cart.actions";
 
-const CartIcon = () => (
-  <div className='cart-icon'>
-    <ShoppingIcon className='shopping-icon'/>
-    <span className='item-count'> 0 </span>
+import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
+
+import "./CartIcon.styles.scss";
+
+const CartIcon = ({ toggleCartHidden }) => (
+  <div className="cart-icon" onClick={toggleCartHidden}>
+    <ShoppingIcon className="shopping-icon" />
+    <span className="item-count"> 0 </span>
   </div>
-)
+);
 
-export default CartIcon;
+const mapDispatchToProps = (dispatch) => ({
+  toggleCartHidden: () => dispatch(toggleCartHidden()),
+});
+export default connect(
+  null,
+  mapDispatchToProps
+  )(CartIcon);
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,34 +2,44 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Header.styles.scss";
 import { ReactComponent as Logo } from "../../assets/original.svg";
-import { auth } from '../../firebase/firebase.utils';
-import { connect } from 'react-redux';
-import CartIcon  from '../cart-icon/CartIcon';
-import CartDropdown from '../cart-dropdown/Cart-Dropdown';
+import { auth } from "../../firebase/firebase.utils";
+import { connect } from "react-redux";
+import CartIcon from "../cart-icon/CartIcon";
+import CartDropdown from "../cart-dropdown/Cart-Dropdown";
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" />
     </Link>
 
     <div className="options">
-      <Link className="option" to="/shop">Shop</Link>
-      <Link className="option" to="/contact">Contact</Link>
-      {currentUser ?
-        <div className="option" onClick={() => auth.signOut()}>Sign Out</div>
-      :
-      <Link className="option" to="/signin">Sign in</Link>
-      }
-    <CartIcon />
+      <Link className="option" to="/shop">
+        Shop
+      </Link>
+      <Link className="option" to="/contact">
+        Contact
+      </Link>
+      {currentUser ? (
+        <div className="option" onClick={() => auth.signOut()}>
+          Sign Out
+        </div>
+      ) : (
+        <Link className="option" to="/signin">
+          Sign in
+        </Link>
+      )}
+
+      <CartIcon />
     </div>
-    <CartDropdown />
+    {!hidden && <CartDropdown />}
   </div>
 );
 
 //function giving access to state > root.reducer
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser
-})
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden,
+});
 
 export default connect(mapStateToProps)(Header);
