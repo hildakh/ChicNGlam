e5ef0c89f9268a867738cbc029c6107b86d90324
Hilda <hilda.kh.amiri@gmail.com>
2020-04-26 15:25:45 -0700

    reducer for adding items to cart added. app data moved to .env file. item was passed as an object to give complete access to it for the cart

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,7 +1,8 @@
 import { CartActionTypes } from './cart.types';
 
 const INITIAL_STATE = {
-  hidden: true
+  hidden: true,
+  cartItems: []
 }
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -11,7 +12,12 @@ const cartReducer = (state = INITIAL_STATE, action) => {
             ...state,
             hidden: !state.hidden
             //payload is not used here so we didn't include it in the cart actions
-      }
+      };
+       case CartActionTypes.ADD_ITEM:
+         return {
+           ...state,
+           cartItems: [...state.cartItems, action.payload]
+         };
         default:
           return state
       }
