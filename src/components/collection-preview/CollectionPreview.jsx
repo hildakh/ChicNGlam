7130cbeaa586b626254f951e3a6bb460226b2501
Hilda <hilda.kh.amiri@gmail.com>
2020-04-25 23:23:55 -0700

    custom button modified to be used on collections items and be visible on hover

--- src/components/collection-preview/CollectionPreview.jsx
+++ src/components/collection-preview/CollectionPreview.jsx
@@ -5,7 +5,7 @@ import CollectionItem from '../collection-item/CollectionItem';
 const CollectionPreview = ({ title, items }) => {
   return (
     <div className="collection-preview">
-      <h1 className="title">{title.toUpperCase()}</h1>
+      <h1 className="title" >{title.toUpperCase()}</h1>
       <div className="preview">
         {items
           .filter((item, index) => index < 4)
