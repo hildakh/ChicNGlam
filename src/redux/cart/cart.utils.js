a167f76dd5d9d53045579bb080ac72de4fe379a1
Hilda <hilda.kh.amiri@gmail.com>
2020-05-03 23:11:56 -0700

    inccreasing quantity of cart items implemented in cart reducer

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -2,7 +2,7 @@ export const addItemToCart = (cartItems, itemToAdd) => {
   const existingCartItem = cartItems.find(cartItem => cartItem.id === itemToAdd.id);
 
   if(existingCartItem) {
-    cartItems.map(cartItem =>
+    return cartItems.map(cartItem =>
       cartItem.id === itemToAdd.id
       && {...cartItem, quantity: cartItem.quantity + 1 });
   }
