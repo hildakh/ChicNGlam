d64cca867d9de735bbf16eefa00d3c7af22af4f9
Hilda <hilda.kh.amiri@gmail.com>
2020-05-07 21:53:02 -0700

    used reselect to update cart dropdown and carticon with memoized selectors

--- src/components/cart-dropdown/Cart-Dropdown.jsx
+++ src/components/cart-dropdown/Cart-Dropdown.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import CartItem from '../cart-item/CartItem';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
 import CustomButton from '../custom-button/CustomButton';
 
 import './Cart-Dropdown.styles.scss';
@@ -20,7 +21,7 @@ const CartDropdown = ({cartItems}) => {
 }
 
 const mapStateToProps = (state) => ({
-  cartItems: state.cart.cartItems,
+  cartItems: selectCartItems(state),
 })
 
 export default connect(mapStateToProps)(CartDropdown);
