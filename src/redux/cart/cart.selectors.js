3ddf04fc1538f990e41e2678eaf9343b06e2007a
Hilda <hilda.kh.amiri@gmail.com>
2020-05-08 18:24:07 -0700

    checkout page updated with items from the cart and the total price

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -16,3 +16,8 @@ export const selectCartHidden = createSelector(
   [selectCart],
   (cart) => cart.hidden
 )
+
+export const selectCartTotalPrice = createSelector(
+  [selectCartItems],
+  cartItems => cartItems.reduce((accumulatedPrice, cartItem) => accumulatedPrice + cartItem.price * cartItem.quantity, 0)
+)
