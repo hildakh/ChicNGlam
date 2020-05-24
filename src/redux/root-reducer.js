85880281379c9d453cf87f38d883fcf6fea63695
hildakh <hilda.kh.amiri@gmail.com>
2020-05-23 23:01:15 -0700

    Redux persist (#44)
    * redux persist installed and added to Readme
    
    * used persist store from redux persist
    
    * config for persistor added. rootReducer was exported with persistReducer. App was wrapped with PersistGate to have access to a persisted state

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,25 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
+// possible config for the redux persist to use
+const persistConfig = {
+  // where inside the reducer object we plan to store everything
+  key: 'root',
+  // same key same value
+  storage,
+  // array of strings of any of the reducers that we want to store, since user is being handled by firebase authentication, no need to persist it >> no need to add it to this array
+  whitelist: ['cart']
+}
 
-export default combineReducers({
+// had to rename combineReducers to rootReducer and wrap it with the persistReducer
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer
-})
+});
+
+//  exported modified version of the rootReducer with persistance capability
+export default persistReducer(persistConfig,rootReducer);
