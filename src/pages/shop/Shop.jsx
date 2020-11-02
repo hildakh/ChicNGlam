b143d8aa0b2ae78c9cfe79f1d5c4f09c7dcdc332
Hilda Kh. Amiri <hilda.kh.amiri@gmail.com>
2020-11-01 22:30:25 -0800

    rendered only collection overview inside shop. moved collection prview mapping to collection overview (#55)
    Co-authored-by: Hilda <hilda.kh.amiri@gmail>

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,23 +1,13 @@
 import React from "react";
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-import CollectionPreview from '../../components/collection-preview/CollectionPreview';
-import { selectCollections } from '../../redux/shop/shop.selectors';
 
-const ShopPage = ({collections}) => {
-    return (
-      <div className='shop-page'>
-        {
-          collections.map(({ id, ...otherCollectionProps}) => (
-            <CollectionPreview key={id} {...otherCollectionProps}/>
-          ))
-        }
-      </div>
-    );
-  }
+import CollectionsOverview from "../../components/collections-overview/CollectionsOverview";
 
-  const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
-  });
+const ShopPage = ({ collections }) => {
+  return (
+    <div className="shop-page">
+      <CollectionsOverview />
+    </div>
+  );
+};
 
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
