1f369a46d065a20ab452e33d68e83ad4dbeed808
Hilda <hilda.kh.amiri@gmail>
2020-11-14 23:09:35 -0800

    collection item used in collection page to render each item. select collection was memoized using lodash memoize

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,3 +1,4 @@
+import memoize from "lodash.memoize";
 import { createSelector } from "reselect";
 
 const COLLECTION_ID_MAP = {
@@ -15,9 +16,14 @@ export const selectCollections = createSelector(
   (shop) => shop.collections
 );
 
-export const selectCollection = (collectionUrlParam) =>
-  createSelector([selectCollections], (collections) =>
-    collections.find(
-      (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
+// By wrapping this function is memoize, we're saying that whenever this function gets called 
+// and receives collectionUrlParam, I want to memoize the return of this function 
+// (in this case we return a selector).If this function gets called again with the same 
+// collectionUrlParam, don't rerun this function because we'll return the same value as last time, 
+// which we've memoized so just return the selector that's been stored.
+export const selectCollection = memoize((collectionUrlParam) =>
+    createSelector(
+        [selectCollections],
+        (collections) => collections[collectionUrlParam]
     )
-  );
+);
