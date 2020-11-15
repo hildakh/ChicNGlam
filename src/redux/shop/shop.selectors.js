fd1f5279c9d04421aae500baf0777109366b17fa
Hilda Kh. Amiri <hilda.kh.amiri@gmail.com>
2020-11-14 23:22:59 -0800

    Feature/collection overview (#56)
    * rendered only collection overview inside shop. moved collection prview mapping to collection overview
    
    * collection selector added to shop selector using collection map
    
    * collection item used in collection page to render each item. select collection was memoized using lodash memoize

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,8 +1,29 @@
+import memoize from "lodash.memoize";
 import { createSelector } from "reselect";
 
-const selectShop = state => state.shop;
+const COLLECTION_ID_MAP = {
+  hats: 1,
+  sneakers: 2,
+  jackets: 3,
+  womens: 4,
+  mens: 5,
+};
+
+const selectShop = (state) => state.shop;
 
 export const selectCollections = createSelector(
-    [selectShop],
-    shop => shop.collections
-)
+  [selectShop],
+  (shop) => shop.collections
+);
+
+// By wrapping this function is memoize, we're saying that whenever this function gets called 
+// and receives collectionUrlParam, I want to memoize the return of this function 
+// (in this case we return a selector).If this function gets called again with the same 
+// collectionUrlParam, don't rerun this function because we'll return the same value as last time, 
+// which we've memoized so just return the selector that's been stored.
+export const selectCollection = memoize((collectionUrlParam) =>
+    createSelector(
+        [selectCollections],
+        (collections) => collections[collectionUrlParam]
+    )
+);
